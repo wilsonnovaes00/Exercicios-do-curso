7bba6780f97bc13f9e3fb4301f42f762eb627403
Wilson Pereira <wilsonnovaes00@hotmail.com>
2015-10-31 16:58:40 -0200

    criando a pasta css e seu respectivo arquivo

--- p1_exercicio1/index.php
+++ p1_exercicio1/index.php
@@ -17,17 +17,53 @@ $j = 21 / 4 / 2;
 
 ?>
 
+<!-- Iniciando o codigo html -->
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" type="text/css" href="estilo.css">
 	<meta charset="UTF-8">
 	<title>Resultado das expressões</title>
 </head>
 <body>
+
 	
-	<center>Resultados das Expressões</center>
+<table dir="ltr" width="477" border="1" class="default">
+<caption>Key languages involved with Web Standards</caption>
+<colgroup width="50%">
+</colgroup><colgroup id="colgroup" class="colgroup" align="center" valign="middle" title="title" width="1*" span="2" style="">
+</colgroup><thead>
+<tr>
+<th scope="col">Language</th>
+<th scope="col">Acronym</th>
+<th scope="col">Purpose</th>
+</tr>
+</thead>
+<tfoot>
+<tr>
+<td>Cascading Style Sheets</td>
+<td>CSS</td>
+<td>Presentation</td>
+</tr>
+</tfoot>
+<tbody>
+<tr>
+<td>Hypertext Markup Language</td>
+<td>HTML</td>
+<td>Structure</td>
+</tr>
+<tr>
+<td>JavaScript</td>
+<td>JS</td>
+<td>Behavior</td>
+</tr>
+</tbody>
+</table>
 
 
 </body>
 </html>
+
+<!-- Fim do código HTML -->
+
