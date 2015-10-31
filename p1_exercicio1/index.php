3aa3a120f58ead52cf670d21dcc9840f69115c9b
Wilson Pereira <wilsonnovaes00@hotmail.com>
2015-10-31 16:35:18 -0200

    Adicionando todas as variaveis e implementando o HTML

--- p1_exercicio1/index.php
+++ p1_exercicio1/index.php
@@ -1,13 +1,33 @@
 <?php
 
-#variaveis
+#Variáveis
 
 $a = 3+4*5;
+$b = 8/4+2*3;
+$c = 2*(10-3*3);
+$d = 5*(3+(2+3))/2+1;
+$e = 1+12/((7+2)/3)+(6-2);
+$f = 3+16/2+5;
+$g = 24 / 4-2;
+$h = 11 % 4+9 / 3;
+$i = sqrt (9)+ sqrt(16);
+$j = 21 / 4 / 2;
 
-
-
-echo $a;
-
-
+#---------------------------------
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Resultado das expressões</title>
+</head>
+<body>
+	
+	<center>Resultados das Expressões</center>
+
+
+</body>
+</html>
