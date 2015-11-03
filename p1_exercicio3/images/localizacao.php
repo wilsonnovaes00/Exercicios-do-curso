0da7ecde190ba28b489bd2142b12677fb2fb6792
root <root@wilson.home>
2015-11-03 13:41:29 -0200

    site completo

---
+++ p1_exercicio3/images/localizacao.php
@@ -0,0 +1,47 @@
+<!-- Incluindo topo --> 
+<?php include_once("topo.php");?>
+
+	<!--  Conteúdo -->
+
+	<div class="row">
+				<div class="text-box col-md-offset-1 col-md-10">
+					<h2>Sobre Nós</h2>
+						<div id="content" class="row">
+							<p> <span>Missão, Visão e Valores</span><br/>
+								<p>A base que norteia cada passo de nossa empresa:</p>
+
+
+							<p>Missão – Prover os melhores cursos online na área de tecnologia, 
+							proporcionando experiências práticas, realistas, e uma base sólida para 
+							seus alunos tornarem-se cada vez mais competitivos no mercado de trabalho.</p>
+
+
+							<p>Visão – Ser uma empresa referência na ministração de cursos inovadores online, 
+							de alta qualidade técnica, na área de tecnologia da informação.</p>
+
+
+							<p><strong>Valores</strong><br/> 
+							– Ética <br/> 
+							– Bom senso <br/> 
+							– Inovação <br/>
+							– Humildade <br />
+							– Responsabilidade </p>
+							<br/>
+					
+					</div>
+				</div>
+				</p>
+				
+			
+
+				</div>
+
+				<div id="nagevacao">Página: <span id="pag">EMPRESA<span></span></div>
+				<br />
+				<br />
+			
+
+
+		
+		<!-- Incluindo footer/Rodapé -->
+			<? include_once("footer.php");?>
