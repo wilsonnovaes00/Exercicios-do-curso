4b2558d6367399744f754f4698d4547235d5004c
Wilso Filho <wilsonnovaes00@hotmail.com>
2015-11-09 17:00:49 -0200

    Colocando a Lógica

--- p2_exercicio1/index.php
+++ p2_exercicio1/index.php
@@ -1,1 +1,27 @@
 <?php
+
+
+
+
+
+
+
+$numeros = array();
+
+
+
+for ($i=0; $i < 5 ; $i++) { 
+	$aleatorio = rand(1, 100);
+	$numeros[$i] = $aleatorio;
+
+	if ($numeros[$i] % 2 == 0) {
+		echo $numeros[$i]. " = Par" .'<br/>';
+	}else{
+		echo $numeros[$i] . " = Impar" .'<br/>';
+	}
+
+	
+
+}
+
+
