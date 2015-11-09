3110cf9211d3bb1e67ef57a7caa4fd7632157752
Wilson <wilsonnovaes00@hotmail.com>
2015-11-09 11:18:59 -0200

    teste

--- p2_exercicio1/index.php
+++ p2_exercicio1/index.php
@@ -2,14 +2,8 @@
 
 echo "teste";
 
-$arrayName = array('' => , );array_diff_assoc(array1<div></div>, array2)
-<div>aa</div>
-udm_alloc_agent_array(
-	)
-
-sem_acquire(
-	udm_alloc_agent_array(
 		)
-	libxml_get_last_error(
 
-		)
+
+
+
