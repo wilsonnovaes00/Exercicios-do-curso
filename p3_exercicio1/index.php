1b744a52222160a555158429c972008bfe882792
Wilson Pereira Novaes Filho <wilsonnovaes00@hotmail.com>
2015-11-21 15:57:43 -0200

    Index add

--- p3_exercicio1/index.php
+++ p3_exercicio1/index.php
@@ -1,1 +1,20 @@
 
+<?php
+session_start();
+include_once './header.php';
+
+if(!isset($_COOKIE["clientes"])){
+    $clientes = [];
+    setcookie("clientes",  serialize($clientes));
+}
+
+echo "<br/>";
+?>
+
+<section class="conteudo">
+    Seja bem vindo
+</section>
+
+<?php
+include_once './footer.php';
+?>
