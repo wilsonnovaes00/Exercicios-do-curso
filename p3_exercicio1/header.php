8b0b48966429b0fa467168134075b4822abdcf07
Wilson Pereira Novaes Filho <wilsonnovaes00@hotmail.com>
2015-11-21 15:58:30 -0200

    Add header

---
+++ p3_exercicio1/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sistema de cadastro</title>
+    </head>
+
+    <h1>Cadastro de Clientes</h1>
+    
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="inserir.php">Inserir</a></li>
+                    <li><a href="listar.php">Listar Clientes</a></li>
+                </ul>
+            </nav>
+        </header>
